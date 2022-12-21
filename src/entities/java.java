c95a160698524e7631c7c467a9bf887cc7894ae7
luizpadilhadev <luizfelipepadilha86@gmail.com>
2022-12-21 19:10:32 -0300

    commit inicial

---
+++ src/entities/java.java
@@ -0,0 +1,13 @@
+package entities;
+
+public class java {
+    private double salario;
+    private int chs;
+    
+    public java(double salario, int chs) {
+        this.salario = salario;
+        this.chs = chs;
+    }
+
+
+}
